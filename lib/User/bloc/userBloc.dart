9926c425507f97b9608555f79cefc44346549a06
Evert Ortiz <evert.ortiz.m@gmail.com>
2022-04-17 21:01:37 -0500

    Valida el stream de la sesion de firebase y redirige al interior de la apliacion

--- lib/User/bloc/userBloc.dart
+++ lib/User/bloc/userBloc.dart
@@ -4,6 +4,8 @@ import 'package:platzi_trips_app/User/repository/authRepository.dart';
 
 class UserBlock implements Bloc{
   final _authRepository = AuthRepository();
+  Stream<User?> streamFirebase = FirebaseAuth.instance.authStateChanges();
+  Stream<User?> get authStatus => streamFirebase;
 
   Future<UserCredential> signIn(){
     return _authRepository.signinFirebase();
--- lib/User/ui/screen/signin.screen.dart
+++ lib/User/ui/screen/signin.screen.dart
@@ -2,8 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_app/User/bloc/userBloc.dart';
+import 'package:platzi_trips_app/appBody.dart';
 import 'package:platzi_trips_app/widgets/buttonGeneric.dart';
 import 'package:platzi_trips_app/widgets/header.dart';
+
+import '../../../Place/ui/screens/homeTrips.dart';
 
 class SigninScreen extends StatefulWidget{
   @override
@@ -20,36 +23,50 @@ class _SigninScreen extends State<StatefulWidget>{
   Widget build(BuildContext context) {
     // TODO: implement build
     this.userBlock = BlocProvider.of(context);
-    return signinGoogleUI();
+    return _handCurrentSession();
   }
 
-
-
+  Widget _handCurrentSession(){
+    return StreamBuilder(
+      stream: userBlock.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snaphot) {
+        if(!snaphot.hasData || snaphot.hasError){
+          return signinGoogleUI();
+        } else {
+          return AppBody();
+        }
+      }
+    );
+  }
+  
   Widget signinGoogleUI(){
     return Scaffold(
       body: Stack(
         children: [
           Header("", MediaQuery.of(context).size.height),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("welcome \n This is your Travel App",
-                style: TextStyle(
-                  fontFamily: "Lato",
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-                )
-              ),
-              ButtonGeneric(
-                  text: "Login with gemail",
-                  onPressed: (){
-                    userBlock.signIn()
-                    .then((UserCredential value) => {
-                      print("El usuario fue creado ${value.user?.displayName}")
-                    });
-                  })
-            ],
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Welcome \nThis is your Travel App",
+                    style: TextStyle(
+                        fontFamily: "Lato",
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    )
+                ),
+                ButtonGeneric(
+                    text: "Login with gmail",
+                    onPressed: (){
+                      userBlock.signIn()
+                          .then((UserCredential value) => {
+                        print("El usuario fue creado ${value.user?.displayName}")
+                      });
+                    })
+              ],
+            ),
           )
         ],
       ),
